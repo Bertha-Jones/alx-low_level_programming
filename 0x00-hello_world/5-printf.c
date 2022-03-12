2dd4b23e3467785513817df5f37cf4145f3862c4
BerthaJones <baajones@st.ug.edu.gh>
2022-03-12 10:14:11 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+- Prints out a string to stdout.
+ * Return: 0 if s#include <stdio.h>
+/**
+ * Description: main uccess.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
