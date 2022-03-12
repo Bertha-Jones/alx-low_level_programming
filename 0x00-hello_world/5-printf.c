a451e283b40ed4f05546e3dd60afd84fb1831521
BerthaJones <baajones@st.ug.edu.gh>
2022-03-12 11:33:40 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main uccess.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper g- Prints out a string to stdout.
- * Return: 0 if sturn (0);
-}rammar, but the outcome is a piece of art,\n");
-return(0)
-	}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
