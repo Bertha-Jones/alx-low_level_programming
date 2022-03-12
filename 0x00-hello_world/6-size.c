38dbc22c29a451492dec45d01229807022e6fa4e
BerthaJones <baajones@st.ug.edu.gh>
2022-03-12 11:40:08 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(voi(char));
+printf("Size of an int: %d byte(s)\n", sid)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", si\n", sizeof(long int));
-printf("Size of a long lonzeof(int));
-printf("Size of a long int: %d byte(s)g int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a char: %d byte(s)\n", sizeofzeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
