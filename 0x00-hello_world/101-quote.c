7ad087c669f8a42758d2c9fee7bcf43af72d1973
BerthaJones <baajones@st.ug.edu.gh>
2022-03-13 13:44:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+* main - Entry point
+*  *
+*   * Return: Always 0 (Succe#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Succeof art is useful\" - Dora Korpar, 2015-10-19\n",
-sss)
+ ss)
  */
 int main(void)
 {
-write(2, "and that piece izeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
