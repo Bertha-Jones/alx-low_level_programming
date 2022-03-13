44ea5c73ff862384729565180f9c21f7b8191e49
BerthaJones <baajones@st.ug.edu.gh>
2022-03-13 13:36:56 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeof art is useful\" - Dora Korpar, 2015-10-19\n",
+sss)
+ */
+int main(void)
+{
+write(2, "and that piece izeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
